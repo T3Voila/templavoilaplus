5cd58abd3d489994f3148bf482d5645823b62f6c
Tolleiv Nietsch <info@tolleiv.de>
2009-11-24 22:56:00 +0000

    Fixed bug: #12721: Cannot get TO with rendertype=print when using DBAL
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/templavoila/trunk@26929 735d13b6-9817-0410-8766-e36946ffe9aa

--- class.tx_templavoila_htmlmarkup.php
+++ class.tx_templavoila_htmlmarkup.php
@@ -670,22 +670,22 @@ class tx_templavoila_htmlmarkup {
 				if ($renderType)	{	// If print-flag try to find a proper print-record. If the lang-uid is also set, try to find a combined print/lang record, but if not found, the print rec. will take precedence.
 
 						// Look up print-row for default language:
-					$printRow = $this->getTemplateRecord_query($parentUid,'AND rendertype="'.addslashes($renderType).'" AND sys_language_uid=0');
+					$printRow = $this->getTemplateRecord_query($parentUid,'AND rendertype=' . $GLOBALS['TYPO3_DB']->fullQuoteStr($renderType, 'tx_templavoila_tmplobj') . ' AND sys_language_uid=0');
 					if (is_array($printRow))	{
 						$rec = $printRow;
 					} elseif ($rendertype_ref) {	// Look in rendertype_ref record:
-						$printRow = $this->getTemplateRecord_query($rendertype_ref['uid'],'AND rendertype="'.addslashes($renderType).'" AND sys_language_uid=0');
+						$printRow = $this->getTemplateRecord_query($rendertype_ref['uid'],'AND rendertype=' . $GLOBALS['TYPO3_DB']->fullQuoteStr($renderType, 'tx_templavoila_tmplobj') . ' AND sys_language_uid=0');
 						if (is_array($printRow))	{
 							$rec = $printRow;
 						}
 					}
 
 					if ($langUid)	{	// If lang_uid is set, try to look up for current language:
-						$printRow = $this->getTemplateRecord_query($parentUid,'AND rendertype="'.addslashes($renderType).'" AND sys_language_uid='.intval($langUid));
+						$printRow = $this->getTemplateRecord_query($parentUid,'AND rendertype=' . $GLOBALS['TYPO3_DB']->fullQuoteStr($renderType, 'tx_templavoila_tmplobj') . ' AND sys_language_uid='.intval($langUid));
 						if (is_array($printRow))	{
 							$rec = $printRow;
 						} elseif ($rendertype_ref) {	// Look in rendertype_ref record:
-							$printRow = $this->getTemplateRecord_query($rendertype_ref['uid'],'AND rendertype="'.addslashes($renderType).'" AND sys_language_uid='.intval($langUid));
+							$printRow = $this->getTemplateRecord_query($rendertype_ref['uid'],'AND rendertype=' . $GLOBALS['TYPO3_DB']->fullQuoteStr($renderType, 'tx_templavoila_tmplobj') . ' AND sys_language_uid='.intval($langUid));
 							if (is_array($printRow))	{
 								$rec = $printRow;
 							}
