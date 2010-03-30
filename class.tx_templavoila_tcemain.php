ad9f80d475cd25feffefb6354eb99853cfc17e16
Tolleiv Nietsch <info@tolleiv.de>
2010-03-30 17:51:26 +0000

    Fixed bug: #13807: sorting CEs via list mode broken if translation 'diff' exists (Thanks to Jan Hacker)
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/templavoila/trunk@31761 735d13b6-9817-0410-8766-e36946ffe9aa

--- class.tx_templavoila_tcemain.php
+++ class.tx_templavoila_tcemain.php
@@ -461,6 +461,9 @@ page.10.disableExplosivePreview = 1
 		$templaVoilaAPI = t3lib_div::makeInstance('tx_templavoila_api');
 		/* @var $templaVoilaAPI tx_templavoila_api */
 
+		$diffBaseEnabled = isset($GLOBALS['TYPO3_CONF_VARS']['BE']['flexFormXMLincludeDiffBase'])
+							&& ($GLOBALS['TYPO3_CONF_VARS']['BE']['flexFormXMLincludeDiffBase'] != false);
+
 			// Getting value of the field containing the relations:
 		$xmlContentArr = t3lib_div::xml2array($flexformXML);
 
@@ -473,6 +476,11 @@ page.10.disableExplosivePreview = 1
 							foreach ($subSubArr as $currentField => $subSubSubArr) {
 								if (is_array ($subSubSubArr)) {
 									foreach ($subSubSubArr as $currentValueKey => $uidList) {
+
+										if($diffBaseEnabled && preg_match('/\.vDEFbase$/', $currentValueKey)) {
+											continue;
+										}
+
 										$uidsArr = t3lib_div::trimExplode (',', $uidList);
 										if (is_array ($uidsArr)) {
 											foreach ($uidsArr as $uid) {
