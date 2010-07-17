7293d174fe56488329231037e43a12b9bdacb670
tolleiv <tolleiv@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-07-17 09:23:08 +0000

    Fixed issue: #15158: XCLASS inclusion inclass.tx_templavoila_label.php wrong
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/templavoila/trunk@35763 735d13b6-9817-0410-8766-e36946ffe9aa

--- classes/class.tx_templavoila_label.php
+++ classes/class.tx_templavoila_label.php
@@ -50,8 +50,8 @@ class tx_templavoila_label {
 	}
 }
 
-if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/templavoila/class.tx_templavoila_label.php'])	{
-	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/templavoila/class.tx_templavoila_label.php']);
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/templavoila/classes/class.tx_templavoila_label.php'])	{
+	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/templavoila/classes/class.tx_templavoila_label.php']);
 }
 
 ?>
