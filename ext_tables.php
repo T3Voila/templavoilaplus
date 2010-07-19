2c155b9c2f75af4d27f898015bb1f628c5008193
tolleiv <tolleiv@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-07-19 19:02:38 +0000

    Fixed issue: #15178: FCE editing forms should match built-in content elements (Thanks to Jeff Segars)
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/templavoila/trunk@35824 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++ ext_tables.php
@@ -186,15 +186,17 @@ $TCA['tt_content']['ctrl']['typeicons'][$_EXTKEY . '_pi1'] = t3lib_extMgm::extRe
 t3lib_extMgm::addPlugin(array('LLL:EXT:templavoila/locallang_db.xml:tt_content.CType_pi1', $_EXTKEY . '_pi1', 'EXT:' . $_EXTKEY . '/icon_fce_ce.png'), 'CType');
 
 if ($_EXTCONF['enable.']['selectDataStructure']) {
-	$TCA['tt_content']['types'][$_EXTKEY . '_pi1']['showitem'] = 'CType;;4;button;1-1-1, header;;3;;2-2-2,tx_templavoila_ds,tx_templavoila_to,tx_templavoila_flex;;;;2-2-2,linkToTop;;;;3-3-3, hidden;;1;;3-3-3';
 	if ($TCA['tt_content']['ctrl']['requestUpdate'] != '') {
 		$TCA['tt_content']['ctrl']['requestUpdate'] .= ',';
 	}
 	$TCA['tt_content']['ctrl']['requestUpdate'] .= 'tx_templavoila_ds';
 }
-else {
-	$TCA['tt_content']['types'][$_EXTKEY . '_pi1']['showitem'] = 'CType;;4;button;1-1-1, header;;3;;2-2-2,tx_templavoila_to,tx_templavoila_flex;;;;2-2-2,linkToTop;;;;3-3-3, hidden;;1;;3-3-3';
-}
+
+$TCA['tt_content']['types'][$_EXTKEY . '_pi1']['showitem'] = 
+	'CType;;4;;1-1-1, hidden, header;;' . (($_EXTCONF['enable.']['renderFCEHeader']) ? '3' : '' ) . ';;2-2-2, linkToTop;;;;3-3-3,
+	 --div--;LLL:EXT:templavoila/locallang_db.xml:tt_content.CType_pi1,' . (($_EXTCONF['enable.']['selectDataStructure']) ? 'tx_templavoila_ds,' : '') . 'tx_templavoila_to,tx_templavoila_flex;;;;2-2-2,
+	 --div--;LLL:EXT:cms/locallang_tca.xml:pages.tabs.access, starttime, endtime, fe_group';
+
 
 	// For pages:
 $tempColumns = array (
