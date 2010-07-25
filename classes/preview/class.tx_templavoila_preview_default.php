6bc4e5236b5214bc6825c593e0ed964c4afb5d6b
tolleiv <tolleiv@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-07-25 23:41:18 +0000

    Fixed bug: #15235: default preview container misses closing strong-tag
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/templavoila/trunk@36049 735d13b6-9817-0410-8766-e36946ffe9aa

--- classes/preview/class.tx_templavoila_preview_default.php
+++ classes/preview/class.tx_templavoila_preview_default.php
@@ -35,7 +35,7 @@ class tx_templavoila_preview_default {
 	 * @return string
 	 */
 	public function render_previewContent ($row, $table, $output, $alreadyRendered, &$ref) {
-		return '<strong>' . $GLOBALS['LANG']->sL(t3lib_BEfunc::getLabelFromItemlist('tt_content', 'CType', $row['CType']));
+		return '<strong>' . $GLOBALS['LANG']->sL(t3lib_BEfunc::getLabelFromItemlist('tt_content', 'CType', $row['CType'])) . '</strong>';
 	}
 }
 
