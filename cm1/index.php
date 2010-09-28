7bd272a6c319aaebcce3048c22329ff32b4cde90
tolleiv <tolleiv@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-09-28 21:45:48 +0000

    Follow-up to: #13974: HTML5 tag support - added missing styles
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/templavoila/trunk@38737 735d13b6-9817-0410-8766-e36946ffe9aa

--- cm1/index.php
+++ cm1/index.php
@@ -202,8 +202,8 @@ class tx_templavoila_cm1 extends t3lib_SCbase {
 	static $gnyfStyleBlock = '
 	.gnyfElement {	color: black; font-family:monospace;font-size:12px !important; line-height:1.3em !important; font-weight:normal; text-transform:none; letter-spacing:auto; cursor: pointer; margin: 0; padding:0 7px; overflow: hidden; text-align: center; position: absolute;  border-radius: 0.4em; -o-border-radius: 0.4em; -moz-border-radius: 0.4em; -webkit-border-radius: 0.4em; background-color: #ffffff;	}
 	span.gnyfElement:hover {	z-index: 100;	box-shadow: rgba(0, 0, 0, 0.5) 0 0 4px 2px;	-o-box-shadow: rgba(0, 0, 0, 0.5) 0 0 4px 2px;	-moz-box-shadow: rgba(0, 0, 0, 0.5) 0 0 4px 2px;	-webkit-box-shadow: rgba(0, 0, 0, 0.5) 0 0 4px 2px;	}
-	a > span.gnyfElement {	position:relative;	}
-	a > .gnyfElement:hover { box-shadow: none;	-o-box-shadow: none;	-moz-box-shadow: none;	-webkit-box-shadow: none;	}	
+	a > span.gnyfElement, td > span.gnyfElement {	position:relative;	}
+	a > .gnyfElement:hover, td > .gnyfElement:hover  { box-shadow: none;	-o-box-shadow: none;	-moz-box-shadow: none;	-webkit-box-shadow: none;	}
 	.gnyfRoot { background-color:#9bff9b; }
 	.gnyfDocument { background-color:#788cff; }
 	.gnyfText { background-color:#ffff64; }
