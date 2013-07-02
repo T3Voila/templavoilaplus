0b28dcbc619b2daa44d3b17b64fe55576b03647c
Wouter Wolters <typo3@wouterwolters.nl>
2013-07-02 18:53:42 +0200

    [TASK] Make backend module load through the dispatcher

--- mod1/conf.php
+++ mod1/conf.php
@@ -1,13 +1,10 @@
 <?php
 
-	// DO NOT REMOVE OR CHANGE THESE 3 LINES:
-define('TYPO3_MOD_PATH', '../typo3conf/ext/templavoila/mod1/');
-$BACK_PATH='../../../../typo3/';
+	// DO NOT REMOVE OR CHANGE THESE 2 LINES:
 $MCONF['name']='web_txtemplavoilaM1';
 
-    
 $MCONF['access']='user,group';
-$MCONF['script']='index.php';
+$MCONF['script']='_DISPATCH';
 
 $MLANG['default']['tabs_images']['tab'] = 'moduleicon.gif';
 $MLANG['default']['ll_ref']='LLL:EXT:templavoila/mod1/locallang_mod.php';
--- mod2/conf.php
+++ mod2/conf.php
@@ -1,13 +1,10 @@
 <?php
 
-	// DO NOT REMOVE OR CHANGE THESE 3 LINES:
-define('TYPO3_MOD_PATH', '../typo3conf/ext/templavoila/mod2/');
-$BACK_PATH='../../../../typo3/';
+	// DO NOT REMOVE OR CHANGE THESE 2 LINES:
 $MCONF['name']='web_txtemplavoilaM2';
 
-    
 $MCONF['access']='user,group';
-$MCONF['script']='index.php';
+$MCONF['script']='_DISPATCH';
 
 $MLANG['default']['tabs_images']['tab'] = 'moduleicon.gif';
 $MLANG['default']['ll_ref']='LLL:EXT:templavoila/mod2/locallang_mod.php';
