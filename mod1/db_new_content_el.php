f43157007f2aa7963acbbe7c6a2bcf60b8e69d9e
Wouter Wolters <typo3@wouterwolters.nl>
2014-07-07 21:39:29 +0200

    [TASK] Re-work/simplify copyright header in PHP files
    This patch replaces the copyright/license header in PHP files with a
    new, simplified one. The new header does not show the year figure, nor
    an author name, and refers to the LICENSE.txt file for the full
    copyright information. License is: GPL2 or any later version.
    
    Releases: 2.0.0
    Change-Id: Iec874cd796d275d164456d48829f0306e4b86c47
    Reviewed-on: https://review.typo3.org/31459
    Reviewed-by: Wouter Wolters
    Tested-by: Wouter Wolters

--- mod1/db_new_content_el.php
+++ mod1/db_new_content_el.php
@@ -1,33 +1,20 @@
 <?php
-/***************************************************************
- *  Copyright notice
+/**
+ * This file is part of the TYPO3 CMS project.
  *
- *  (c) 2003-2006 Robert Lemke (robert@typo3.org)
- *  All rights reserved
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
  *
- *  This script is part of the TYPO3 project. The TYPO3 project is
- *  free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
  *
- *  The GNU General Public License can be found at
- *  http://www.gnu.org/copyleft/gpl.html.
- *  A copy is found in the textfile GPL.txt and important notices to the license
- *  from the author is found in LICENSE.txt distributed with these scripts.
- *
- *
- *  This script is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  This copyright notice MUST APPEAR in all copies of the script!
- ***************************************************************/
+ * The TYPO3 project - inspiring people to share!
+ */
+
 /**
  * New content elements wizard for templavoila
  *
- * $Id$
  * Originally based on the CE wizard / cms extension by Kasper Skaarhoj <kasper@typo3.com>
  * XHTML compatible.
  *
