f41e4535af19d394748f7accc425a1c3bdc91553
Christian Kuhn <lolli@schwarzbu.ch>
2015-01-25 23:19:13 +0100

    [BUGFIX] makeInstance is called with \ in front
    Probably a bit WIP, it should be checked if there are more
    occurrences. Backend module "TV page" came up again with patch.
    
    Resolves: #64497
    Change-Id: I710c9c80267fe5a8bdc8902d1ba9388a676dbdc4
    Reviewed-on: http://review.typo3.org/36255
    Reviewed-by: Wouter Wolters <typo3@wouterwolters.nl>
    Tested-by: Wouter Wolters <typo3@wouterwolters.nl>

--- ext_localconf.php
+++ ext_localconf.php
@@ -44,35 +44,35 @@ $tvSetup[] = 'tt_content.menu.20.3 = USER
 $GLOBALS['TYPO3_CONF_VARS']['FE']['addRootLineFields'] .= ',tx_templavoila_ds,tx_templavoila_to,tx_templavoila_next_ds,tx_templavoila_next_to';
 
 // Register our classes at a the hooks:
-$GLOBALS ['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass']['templavoila'] = 'EXT:templavoila/Classes/Service/DataHandling/DataHandler.php:\Extension\Templavoila\Service\DataHandling\DataHandler';
-$GLOBALS ['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processCmdmapClass']['templavoila'] = 'EXT:templavoila/Classes/Service/DataHandling/DataHandler.php:\Extension\Templavoila\Service\DataHandling\DataHandler';
-$GLOBALS ['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['moveRecordClass']['templavoila'] = 'EXT:templavoila/Classes/Service/DataHandling/DataHandler.php:\Extension\Templavoila\Service\DataHandling\DataHandler';
-$GLOBALS ['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_userauthgroup.php']['recordEditAccessInternals']['templavoila'] = 'EXT:templavoila/Classes/Service/UserFunc/Access.php:&\Extension\Templavoila\Service\UserFunc\Access->recordEditAccessInternals';
+$GLOBALS ['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass']['templavoila'] = 'EXT:templavoila/Classes/Service/DataHandling/DataHandler.php:Extension\Templavoila\Service\DataHandling\DataHandler';
+$GLOBALS ['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processCmdmapClass']['templavoila'] = 'EXT:templavoila/Classes/Service/DataHandling/DataHandler.php:Extension\Templavoila\Service\DataHandling\DataHandler';
+$GLOBALS ['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['moveRecordClass']['templavoila'] = 'EXT:templavoila/Classes/Service/DataHandling/DataHandler.php:Extension\Templavoila\Service\DataHandling\DataHandler';
+$GLOBALS ['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_userauthgroup.php']['recordEditAccessInternals']['templavoila'] = 'EXT:templavoila/Classes/Service/UserFunc/Access.php:&Extension\Templavoila\Service\UserFunc\Access->recordEditAccessInternals';
 
-$GLOBALS ['TYPO3_CONF_VARS']['EXTCONF']['lowlevel']['cleanerModules']['tx_templavoila_unusedce'] = array('EXT:templavoila/Classes/Comand/UnusedContentElementComand.php:\Extension\Templavoila\Comand\UnusedContentElementComand');
-$GLOBALS ['TYPO3_CONF_VARS']['EXTCONF']['l10nmgr']['indexFilter']['tx_templavoila_usedCE'] = array('EXT:templavoila/Classes/Service/UserFunc/UsedContentElement.php:\Extension\Templavoila\Service\UserFunc\UsedContentElement');
+$GLOBALS ['TYPO3_CONF_VARS']['EXTCONF']['lowlevel']['cleanerModules']['tx_templavoila_unusedce'] = array('EXT:templavoila/Classes/Comand/UnusedContentElementComand.php:Extension\Templavoila\Comand\UnusedContentElementComand');
+$GLOBALS ['TYPO3_CONF_VARS']['EXTCONF']['l10nmgr']['indexFilter']['tx_templavoila_usedCE'] = array('EXT:templavoila/Classes/Service/UserFunc/UsedContentElement.php:Extension\Templavoila\Service\UserFunc\UsedContentElement');
 
 // Register Preview Classes for Page Module
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['templavoila']['mod1']['renderPreviewContent']['default'] = 'EXT:templavoila/Classes/Controller/Preview/DefaultController.php:&\Extension\Templavoila\Controller\Preview\DefaultController';
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['templavoila']['mod1']['renderPreviewContent']['text'] = 'EXT:templavoila/Classes/Controller/Preview/TextController.php:&\Extension\Templavoila\Controller\Preview\TextController';
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['templavoila']['mod1']['renderPreviewContent']['table'] = 'EXT:templavoila/Classes/Controller/Preview/TextController.php:&\Extension\Templavoila\Controller\Preview\TextController';
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['templavoila']['mod1']['renderPreviewContent']['mailform'] = 'EXT:templavoila/Classes/Controller/Preview/TextController.php:&\Extension\Templavoila\Controller\Preview\TextController';
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['templavoila']['mod1']['renderPreviewContent']['header'] = 'EXT:templavoila/Classes/Controller/Preview/HeaderController.php:&\Extension\Templavoila\Controller\Preview\HeaderController';
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['templavoila']['mod1']['renderPreviewContent']['multimedia'] = 'EXT:templavoila/Classes/Controller/Preview/MultimediaController.php:&\Extension\Templavoila\Controller\Preview\MultimediaController';
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['templavoila']['mod1']['renderPreviewContent']['media'] = 'EXT:templavoila/Classes/Controller/Preview/MediaController.php:&\Extension\Templavoila\Controller\Preview\MediaController';
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['templavoila']['mod1']['renderPreviewContent']['uploads'] = 'EXT:templavoila/Classes/Controller/Preview/UploadsController.php:&\Extension\Templavoila\Controller\Preview\UploadsController';
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['templavoila']['mod1']['renderPreviewContent']['textpic'] = 'EXT:templavoila/Classes/Controller/Preview/TextpicController.php:&\Extension\Templavoila\Controller\Preview\TextpicController';
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['templavoila']['mod1']['renderPreviewContent']['splash'] = 'EXT:templavoila/Classes/Controller/Preview/TextpicController.php:&\Extension\Templavoila\Controller\Preview\TextpicController';
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['templavoila']['mod1']['renderPreviewContent']['image'] = 'EXT:templavoila/Classes/Controller/Preview/ImageController.php:&\Extension\Templavoila\Controller\Preview\ImageController';
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['templavoila']['mod1']['renderPreviewContent']['bullets'] = 'EXT:templavoila/Classes/Controller/Preview/BulletsController.php:&\Extension\Templavoila\Controller\Preview\BulletsController';
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['templavoila']['mod1']['renderPreviewContent']['html'] = 'EXT:templavoila/Classes/Controller/Preview/HtmlController.php:&\Extension\Templavoila\Controller\Preview\HtmlController';
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['templavoila']['mod1']['renderPreviewContent']['menu'] = 'EXT:templavoila/Classes/Controller/Preview/MenuController.php:&\Extension\Templavoila\Controller\Preview\MenuController';
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['templavoila']['mod1']['renderPreviewContent']['list'] = 'EXT:templavoila/Classes/Controller/Preview/ListController.php:&\Extension\Templavoila\Controller\Preview\ListController';
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['templavoila']['mod1']['renderPreviewContent']['search'] = 'EXT:templavoila/Classes/Controller/Preview/NullController.php:&\Extension\Templavoila\Controller\Preview\NullController';
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['templavoila']['mod1']['renderPreviewContent']['login'] = 'EXT:templavoila/Classes/Controller/Preview/NullController.php:&\Extension\Templavoila\Controller\Preview\NullController';
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['templavoila']['mod1']['renderPreviewContent']['shortcut'] = 'EXT:templavoila/Classes/Controller/Preview/NullController.php:&\Extension\Templavoila\Controller\Preview\NullController';
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['templavoila']['mod1']['renderPreviewContent']['div'] = 'EXT:templavoila/Classes/Controller/Preview/NullController.php:&\Extension\Templavoila\Controller\Preview\NullController';
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['templavoila']['mod1']['renderPreviewContent']['templavoila_pi1'] = 'EXT:templavoila/Classes/Controller/Preview/NullController.php:&\Extension\Templavoila\Controller\Preview\NullController';
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['templavoila']['mod1']['renderPreviewContent']['default'] = 'EXT:templavoila/Classes/Controller/Preview/DefaultController.php:&Extension\Templavoila\Controller\Preview\DefaultController';
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['templavoila']['mod1']['renderPreviewContent']['text'] = 'EXT:templavoila/Classes/Controller/Preview/TextController.php:&Extension\Templavoila\Controller\Preview\TextController';
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['templavoila']['mod1']['renderPreviewContent']['table'] = 'EXT:templavoila/Classes/Controller/Preview/TextController.php:&Extension\Templavoila\Controller\Preview\TextController';
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['templavoila']['mod1']['renderPreviewContent']['mailform'] = 'EXT:templavoila/Classes/Controller/Preview/TextController.php:&Extension\Templavoila\Controller\Preview\TextController';
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['templavoila']['mod1']['renderPreviewContent']['header'] = 'EXT:templavoila/Classes/Controller/Preview/HeaderController.php:&Extension\Templavoila\Controller\Preview\HeaderController';
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['templavoila']['mod1']['renderPreviewContent']['multimedia'] = 'EXT:templavoila/Classes/Controller/Preview/MultimediaController.php:&Extension\Templavoila\Controller\Preview\MultimediaController';
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['templavoila']['mod1']['renderPreviewContent']['media'] = 'EXT:templavoila/Classes/Controller/Preview/MediaController.php:&Extension\Templavoila\Controller\Preview\MediaController';
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['templavoila']['mod1']['renderPreviewContent']['uploads'] = 'EXT:templavoila/Classes/Controller/Preview/UploadsController.php:&Extension\Templavoila\Controller\Preview\UploadsController';
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['templavoila']['mod1']['renderPreviewContent']['textpic'] = 'EXT:templavoila/Classes/Controller/Preview/TextpicController.php:&Extension\Templavoila\Controller\Preview\TextpicController';
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['templavoila']['mod1']['renderPreviewContent']['splash'] = 'EXT:templavoila/Classes/Controller/Preview/TextpicController.php:&Extension\Templavoila\Controller\Preview\TextpicController';
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['templavoila']['mod1']['renderPreviewContent']['image'] = 'EXT:templavoila/Classes/Controller/Preview/ImageController.php:&Extension\Templavoila\Controller\Preview\ImageController';
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['templavoila']['mod1']['renderPreviewContent']['bullets'] = 'EXT:templavoila/Classes/Controller/Preview/BulletsController.php:&Extension\Templavoila\Controller\Preview\BulletsController';
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['templavoila']['mod1']['renderPreviewContent']['html'] = 'EXT:templavoila/Classes/Controller/Preview/HtmlController.php:&Extension\Templavoila\Controller\Preview\HtmlController';
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['templavoila']['mod1']['renderPreviewContent']['menu'] = 'EXT:templavoila/Classes/Controller/Preview/MenuController.php:&Extension\Templavoila\Controller\Preview\MenuController';
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['templavoila']['mod1']['renderPreviewContent']['list'] = 'EXT:templavoila/Classes/Controller/Preview/ListController.php:&Extension\Templavoila\Controller\Preview\ListController';
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['templavoila']['mod1']['renderPreviewContent']['search'] = 'EXT:templavoila/Classes/Controller/Preview/NullController.php:&Extension\Templavoila\Controller\Preview\NullController';
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['templavoila']['mod1']['renderPreviewContent']['login'] = 'EXT:templavoila/Classes/Controller/Preview/NullController.php:&Extension\Templavoila\Controller\Preview\NullController';
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['templavoila']['mod1']['renderPreviewContent']['shortcut'] = 'EXT:templavoila/Classes/Controller/Preview/NullController.php:&Extension\Templavoila\Controller\Preview\NullController';
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['templavoila']['mod1']['renderPreviewContent']['div'] = 'EXT:templavoila/Classes/Controller/Preview/NullController.php:&Extension\Templavoila\Controller\Preview\NullController';
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['templavoila']['mod1']['renderPreviewContent']['templavoila_pi1'] = 'EXT:templavoila/Classes/Controller/Preview/NullController.php:&Extension\Templavoila\Controller\Preview\NullController';
 
 $GLOBALS['TYPO3_CONF_VARS']['BE']['AJAX']['tx_templavoila_mod1_ajax::moveRecord'] =
 	'EXT:templavoila/mod1/class.tx_templavoila_mod1_ajax.php:tx_templavoila_mod1_ajax->moveRecord';
