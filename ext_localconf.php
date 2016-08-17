8afe50f930d60e0aafff6676b9d015a4e87cd815
Alexander Opitz <opitz@pluspol.info>
2016-08-17 13:23:53 +0200

    Add language aware FlexForm handling from compatibility6 extension: https://github.com/FriendsOfTYPO3/compatibility6

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,8 +1,51 @@
 <?php
 defined('TYPO3_MODE') or die();
-
 // Unserializing the configuration so we can use it here
 $_EXTCONF = unserialize($_EXTCONF);
+
+// Register language aware flex form handling in FormEngine
+// Register render elements
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1443361297] = [
+    'nodeName' => 'flex',
+    'priority' => 40,
+    'class' => \Extension\Templavoila\Form\Container\FlexFormEntryContainer::class,
+];
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1443361298] = [
+    'nodeName' => 'flexFormNoTabsContainer',
+    'priority' => 40,
+    'class' => \Extension\Templavoila\Form\Container\FlexFormNoTabsContainer::class,
+];
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1443361299] = [
+    'nodeName' => 'flexFormTabsContainer',
+    'priority' => 40,
+    'class' => \Extension\Templavoila\Form\Container\FlexFormTabsContainer::class,
+];
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1443361300] = [
+    'nodeName' => 'flexFormElementContainer',
+    'priority' => 40,
+    'class' => \Extension\Templavoila\Form\Container\FlexFormElementContainer::class,
+];
+// Unregister stock TcaFlexProcess data provider and substitute with own data provider at the same position
+unset($GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['formDataGroup']['tcaDatabaseRecord']
+    [\TYPO3\CMS\Backend\Form\FormDataProvider\TcaFlexProcess::class]
+);
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['formDataGroup']['tcaDatabaseRecord']
+    [\Extension\Templavoila\Form\FormDataProvider\TcaFlexProcess::class] = [
+        'depends' => [
+            \TYPO3\CMS\Backend\Form\FormDataProvider\TcaFlexPrepare::class,
+        ]
+    ];
+unset($GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['formDataGroup']['tcaDatabaseRecord']
+    [\TYPO3\CMS\Backend\Form\FormDataProvider\TcaRadioItems::class]['depends'][\TYPO3\CMS\Backend\Form\FormDataProvider\TcaFlexProcess::class]
+);
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['formDataGroup']['tcaDatabaseRecord']
+    [\TYPO3\CMS\Backend\Form\FormDataProvider\TcaRadioItems::class]['depends'][]
+        = \Extension\Templavoila\Form\FormDataProvider\TcaFlexProcess::class;
+// Register "XCLASS" of FlexFormTools for language parsing
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\TYPO3\CMS\Core\Configuration\FlexForm\FlexFormTools::class]['className']
+    = \Extension\Templavoila\Configuration\FlexForm\FlexFormTools::class;
+// Language diff updating in flex
+$GLOBALS['TYPO3_CONF_VARS']['BE']['flexFormXMLincludeDiffBase'] = true;
 
 // Adding the two plugins TypoScript:
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript('templavoila', 'setup', '
