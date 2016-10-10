5ca3e576f6a4642a61293177b59295d6e1931a44
Alexander Opitz <opitz@pluspol.info>
2016-10-10 17:49:58 +0200

    Fix CS a bit

--- mod1/dragdrop.js
+++ mod1/dragdrop.js
@@ -1,57 +1,57 @@
 var browserPos = null;
 
 function setFormValueOpenBrowser(url, mode, params) {
-	var url = url + "&mode=" + mode + "&bparams=" + params;
+    var url = url + "&mode=" + mode + "&bparams=" + params;
 
-	browserWin = window.open(url, "templavoilareferencebrowser", "height=350,width=" + (mode == "db" ? 650 : 600) + ",status=0,menubar=0,resizable=1,scrollbars=1");
-	browserWin.focus();
+    browserWin = window.open(url, "templavoilareferencebrowser", "height=350,width=" + (mode == "db" ? 650 : 600) + ",status=0,menubar=0,resizable=1,scrollbars=1");
+    browserWin.focus();
 }
 
 function setFormValueFromBrowseWin(fName, value, label, exclusiveValues) {
-	if (value) {
-		var ret = value.split('_');
-		var rid = ret.pop();
-		ret = ret.join('_');
-		browserPos.href = browserPos.rel.replace('%23%23%23', ret + ':' + rid);
-		jumpToUrl(browserPos.href);
-	}
+    if (value) {
+        var ret = value.split('_');
+        var rid = ret.pop();
+        ret = ret.join('_');
+        browserPos.href = browserPos.rel.replace('%23%23%23', ret + ':' + rid);
+        jumpToUrl(browserPos.href);
+    }
 }
 
 function jumpToUrl(URL) {
-	window.location.href = URL;
-	return false;
+    window.location.href = URL;
+    return false;
 }
 
 function setHighlight(id) {
-	top.fsMod.recentIds["web"] = id;
-	top.fsMod.navFrameHighlightedID["web"] = "pages" + id + "_" + top.fsMod.currentBank;	// For highlighting
+    top.fsMod.recentIds["web"] = id;
+    top.fsMod.navFrameHighlightedID["web"] = "pages" + id + "_" + top.fsMod.currentBank;	// For highlighting
 
-	if (top.content && top.content.nav_frame && top.content.nav_frame.refresh_nav) {
-		top.content.nav_frame.refresh_nav();
-	}
+    if (top.content && top.content.nav_frame && top.content.nav_frame.refresh_nav) {
+        top.content.nav_frame.refresh_nav();
+    }
 }
 
 function editRecords(table, idList, addParams, CBflag) {
-	window.location.href = T3_TV_MOD1_BACKPATH + "alt_doc.php?returnUrl=" + T3_TV_MOD1_RETURNURL + "&edit[" + table + "][" + idList + "]=edit" + addParams;
+    window.location.href = T3_TV_MOD1_BACKPATH + "alt_doc.php?returnUrl=" + T3_TV_MOD1_RETURNURL + "&edit[" + table + "][" + idList + "]=edit" + addParams;
 }
 function editList(table, idList) {
-	var list = "";
+    var list = "";
 
-	// Checking how many is checked, how many is not
-	var pointer = 0;
-	var pos = idList.indexOf(",");
-	while (pos != -1) {
-		if (cbValue(table + "|" + idList.substr(pointer, pos - pointer))) {
-			list += idList.substr(pointer, pos - pointer) + ",";
-		}
-		pointer = pos + 1;
-		pos = idList.indexOf(",", pointer);
-	}
-	if (cbValue(table + "|" + idList.substr(pointer))) {
-		list += idList.substr(pointer) + ",";
-	}
+    // Checking how many is checked, how many is not
+    var pointer = 0;
+    var pos = idList.indexOf(",");
+    while (pos != -1) {
+        if (cbValue(table + "|" + idList.substr(pointer, pos - pointer))) {
+            list += idList.substr(pointer, pos - pointer) + ",";
+        }
+        pointer = pos + 1;
+        pos = idList.indexOf(",", pointer);
+    }
+    if (cbValue(table + "|" + idList.substr(pointer))) {
+        list += idList.substr(pointer) + ",";
+    }
 
-	return list ? list : idList;
+    return list ? list : idList;
 }
 
 // --- drag & drop ----
@@ -61,29 +61,29 @@ var sortable_currentItem;
 // sortable_linkParameters = mod1/index.php -- $this->link_getParameters()
 
 function sortable_unhideRecord(it, command) {
-	jumpToUrl(command);
+    jumpToUrl(command);
 }
 
 function sortable_hideRecord(it, command) {
-	if (!sortable_removeHidden) {
-		return jumpToUrl(command);
-	}
+    if (!sortable_removeHidden) {
+        return jumpToUrl(command);
+    }
 
-	while ((typeof it.className == "undefined") || (it.className.search(/tpm-element(?!-)/) == -1)) {
-		it = it.parentNode;
-	}
-	new Ajax.Request(command);
-	new Effect.Fade(it,
-		{ duration: 0.5,
-			afterFinish: sortable_hideRecordCallBack });
+    while ((typeof it.className == "undefined") || (it.className.search(/tpm-element(?!-)/) == -1)) {
+        it = it.parentNode;
+    }
+    new Ajax.Request(command);
+    new Effect.Fade(it,
+        { duration: 0.5,
+            afterFinish: sortable_hideRecordCallBack });
 }
 
 function sortable_hideRecordCallBack(obj) {
-	var el = obj.element;
+    var el = obj.element;
 
-	while (el.lastChild) {
-		el.removeChild(el.lastChild);
-	}
+    while (el.lastChild) {
+        el.removeChild(el.lastChild);
+    }
 }
 
 function sortable_unlinkRecordCallBack(obj)
@@ -201,27 +201,27 @@ function sortable_stop()
 
 function tv_createSortable(container, connectWith)
 {
-        var $sortingContainer = TYPO3.jQuery(container);
-        $sortingContainer.sortable(
-        {
-            connectWith: connectWith,
-            handle: '.sortable_handle',
-            items: '.sortableItem',
-            //zIndex: '4000',
-            tolerance: 'pointer',
-            opacity: 0.5,
-            revert: true,
-            start: function (event, ui) {
-                sortable_start(TYPO3.jQuery(this)[0], ui.item[0], TYPO3.jQuery(this).sortable('toArray'));
-            },
-            update: function (event, ui) {
-                sortable_update(TYPO3.jQuery(this)[0], ui.item[0], TYPO3.jQuery(this).sortable('toArray'));
-            },
-            stop: function (event, ui) {
-                sortable_stop();
-            },
-            forcePlaceholderSize: true,
-            placeholder: 'drag-placeholder'
-        });
-        $sortingContainer.disableSelection();
+    var $sortingContainer = TYPO3.jQuery(container);
+    $sortingContainer.sortable(
+    {
+        connectWith: connectWith,
+        handle: '.sortable_handle',
+        items: '.sortableItem',
+        //zIndex: '4000',
+        tolerance: 'pointer',
+        opacity: 0.5,
+        revert: true,
+        start: function (event, ui) {
+            sortable_start(TYPO3.jQuery(this)[0], ui.item[0], TYPO3.jQuery(this).sortable('toArray'));
+        },
+        update: function (event, ui) {
+            sortable_update(TYPO3.jQuery(this)[0], ui.item[0], TYPO3.jQuery(this).sortable('toArray'));
+        },
+        stop: function (event, ui) {
+            sortable_stop();
+        },
+        forcePlaceholderSize: true,
+        placeholder: 'drag-placeholder'
+    });
+    $sortingContainer.disableSelection();
 }
