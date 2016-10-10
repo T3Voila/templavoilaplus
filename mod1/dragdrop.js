36b5960b5592c3e19dbff16610f8f7af920045d6
Alexander Opitz <opitz@pluspol.info>
2016-10-10 17:48:24 +0200

    Remove unused jump functions in JS

--- mod1/dragdrop.js
+++ mod1/dragdrop.js
@@ -21,15 +21,6 @@ function jumpToUrl(URL) {
 	window.location.href = URL;
 	return false;
 }
-function jumpExt(URL, anchor) {
-	var anc = anchor ? anchor : "";
-	window.location.href = URL + (T3_THIS_LOCATION ? "&returnUrl=" + T3_THIS_LOCATION : "") + anc;
-	return false;
-}
-function jumpSelf(URL) {
-	window.location.href = URL + (T3_RETURN_URL ? "&returnUrl=" + T3_RETURN_URL : "");
-	return false;
-}
 
 function setHighlight(id) {
 	top.fsMod.recentIds["web"] = id;
