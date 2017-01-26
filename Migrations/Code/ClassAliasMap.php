5c4ba9c49c6de79101f88efa8a1f3f75652edf13
Andreas Kiessling <kiessling@pluspol-interactive.de>
2017-01-26 15:34:19 +0100

    Add class alias for tx_templavoila_module1

--- Migrations/Code/ClassAliasMap.php
+++ Migrations/Code/ClassAliasMap.php
@@ -1,5 +1,6 @@
 <?php
 
 return [
-    'tx_templavoila_pi1' => \Extension\Templavoila\Controller\FrontendController::class
+    'tx_templavoila_pi1' => \Extension\Templavoila\Controller\FrontendController::class,
+    'tx_templavoila_module1' => \Extension\Templavoila\Controller\BackendLayoutController::class,
 ];
