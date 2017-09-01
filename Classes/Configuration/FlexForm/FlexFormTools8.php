0680fbd704b1091235655d4184c088a512db7a97
Alexander Opitz <opitz@pluspol.info>
2017-09-01 16:32:56 +0200

    [TASK] v8: Implement traverseFlexFormXMLData for v8/v9
    Implement it from TYPO3 v8 core so differences are lower then using the
    v7 implementation.
    
    Resolves: #117

--- Classes/Configuration/FlexForm/FlexFormTools8.php
+++ Classes/Configuration/FlexForm/FlexFormTools8.php
@@ -118,4 +118,79 @@ class FlexFormTools8 extends FlexFormTools
             return parent::parseDataStructureByIdentifier($identifier);
         }
     }
+
+
+    /**
+     * Handler for Flex Forms
+     *
+     * @param string $table The table name of the record
+     * @param string $field The field name of the flexform field to work on
+     * @param array $row The record data array
+     * @param object $callBackObj Object in which the call back function is located
+     * @param string $callBackMethod_value Method name of call back function in object for values
+     * @return bool|string true on success, string if error happened (error string returned)
+     */
+    public function traverseFlexFormXMLData($table, $field, $row, $callBackObj, $callBackMethod_value)
+    {
+        if (!is_array($GLOBALS['TCA'][$table]) || !is_array($GLOBALS['TCA'][$table]['columns'][$field])) {
+            return 'TCA table/field was not defined.';
+        }
+        $this->callBackObj = $callBackObj;
+        // Get Data Structure:
+        $dataStructureIdentifier = $this->getDataStructureIdentifier($GLOBALS['TCA'][$table]['columns'][$field], $table, $field, $row);
+        $dataStructureArray = $this->parseDataStructureByIdentifier($dataStructureIdentifier);
+        // Get flexform XML data
+        $editData = GeneralUtility::xml2array($row[$field]);
+        if (!is_array($editData)) {
+            return 'Parsing error: ' . $editData;
+        }
+        // Check if $dataStructureArray['sheets'] is indeed an array before loop or it will crash with runtime error
+        if (!is_array($dataStructureArray['sheets'])) {
+            return 'Data Structure ERROR: sheets is defined but not an array for table ' . $table . (isset($row['uid']) ? ' and uid ' . $row['uid'] : '');
+        }
+
+        // Language settings:
+        $langChildren = 0;
+        $langDisabled = 0;
+        if (isset($dataStructArray['meta'])) {
+            $langChildren = $dataStructArray['meta']['langChildren'] ? 1 : 0;
+            $langDisabled = $dataStructArray['meta']['langDisable'] ? 1 : 0;
+        }
+
+        // Empty or invalid <meta>
+        if (!isset($editData['meta']) || !is_array($editData['meta'])) {
+            $editData['meta'] = [];
+        }
+        $editData['meta']['currentLangId'] = [];
+        $languages = $this->getAvailableLanguages();
+        foreach ($languages as $lInfo) {
+            $editData['meta']['currentLangId'][] = $lInfo['ISOcode'];
+        }
+        $editData['meta']['currentLangId'] = array_unique($editData['meta']['currentLangId']);
+        if ($langChildren || $langDisabled) {
+            $lKeys = ['DEF'];
+        } else {
+            $lKeys = $editData['meta']['currentLangId'];
+        }
+
+        // Traverse languages:
+        foreach ($dataStructureArray['sheets'] as $sheetKey => $sheetData) {
+            // Render sheet:
+            if (is_array($sheetData['ROOT']) && is_array($sheetData['ROOT']['el'])) {
+                $lang = 'l' . $lKey;
+
+                $PA['vKeys'] = $langChildren && !$langDisabled ? $editData['meta']['currentLangId'] : ['DEF'];
+                $PA['lKey'] = $lang;
+                $PA['callBackMethod_value'] = $callBackMethod_value;
+                $PA['table'] = $table;
+                $PA['field'] = $field;
+                $PA['uid'] = $row['uid'];
+                // Render flexform:
+                $this->traverseFlexFormXMLData_recurse($sheetData['ROOT']['el'], $editData['data'][$sheetKey][$lang], $PA, 'data/' . $sheetKey . '/' . $lang);
+            } else {
+                return 'Data Structure ERROR: No ROOT element found for sheet "' . $sheetKey . '".';
+            }
+        }
+        return true;
+    }
 }
