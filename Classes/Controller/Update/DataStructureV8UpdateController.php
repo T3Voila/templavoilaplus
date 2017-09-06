a15f2888cceae67f125ffecde8be03d2c3415f3a
Alexander Opitz <opitz@pluspol.info>
2017-09-06 12:27:14 +0200

    [BUGFIX] v8: migrate defaultExtras TCA configuration
    The richtext (and others) configuration in defaultExtras changed with
    TYPO3 v8 https://forge.typo3.org/issues/72856
    We need to migrate this and use new default values for eType rte.
    
    Resolves: #119

--- Classes/Controller/Update/DataStructureV8UpdateController.php
+++ Classes/Controller/Update/DataStructureV8UpdateController.php
@@ -35,7 +35,129 @@ class DataStructureV8UpdateController extends StepUpdateController
 
     protected function stepFinal()
     {
+        if ($this->extConf['staticDS.']['enable']
+            && version_compare(TYPO3_version, '8.5.0', '>=')
+        ) {
+            // If static DS is in use and TYPO3 is newer then 8.5.0 we need to migrate the file pointer
+            $countStatic = $this->migrateStaticDsFilePointer();
+        }
 
+        $handler = GeneralUtility::makeInstance(DataStructureUpdateHandler::class);
+        $count = $handler->updateAllDs(
+            [],
+            [
+                [$this, 'migrateDefaultExtrasRteTransFormOptions'],
+                [$this, 'migrateLastPiecesOfDefaultExtras'],
+                [$this, 'cleanupEmptyDefaultExtraFields'],
+            ]
+        );
+
+        $this->fluid->assignMultiple([
+            'countStatic' => $countStatic,
+            'count' => $count,
+            'errors' => $this->errors,
+        ]);
+    }
+
+    /**
+     * Migrate defaultExtras "richtext:rte_transform[mode=ts_css]" and similar stuff like
+     * "richtext:rte_transform[mode=ts_css]" to "richtext:rte_transform"
+     * From TYPO3 8 LTS
+     *   TYPO3\CMS\Core\Migrations\TcaMigration::migrateDefaultExtrasRteTransFormOptions
+     *
+     * @param array $element The field element TCA
+     * @return boolean True if changed otherwise false
+     */
+    public function migrateDefaultExtrasRteTransFormOptions(array &$element)
+    {
+        $changed = false;
+        if (
+            isset($element['TCEforms']['defaultExtras']) // if defaultExtras is set
+        ) {
+            $defaultExtrasArray = GeneralUtility::trimExplode(':', $element['TCEforms']['defaultExtras'], true);
+            foreach ($defaultExtrasArray as $part => $defaultExtrasField) {
+                if (substr($defaultExtrasField, 0, 8) === 'richtext') {
+                    $element['TCEforms']['config']['enableRichtext'] = true;
+                    $element['TCEforms']['config']['richtextConfiguration'] = 'default';
+                    unset($defaultExtrasArray[$part]);
+                    $changed = true;
+                } elseif (substr($defaultExtrasField, 0, 13) === 'rte_transform') {
+                    unset($defaultExtrasArray[$part]);
+                    $changed = true;
+                }
+            }
+        }
+
+        if ($changed) {
+            $element['TCEforms']['defaultExtras'] = implode(':', $defaultExtrasArray);
+        }
+
+        return $changed;
+    }
+
+    /**
+     * Migrate defaultExtras "nowrap", "enable-tab", "fixed-font". Then drop all
+     * remaining "defaultExtras", there shouldn't exist anymore.
+     * From TYPO3 8 LTS
+     *   TYPO3\CMS\Core\Migrations\TcaMigration::migrateLastPiecesOfDefaultExtras
+     *
+     * @param array $tca
+     * @return array
+     */
+    public function migrateLastPiecesOfDefaultExtras(array &$element)
+    {
+        $changed = false;
+        if (
+            isset($element['TCEforms']['defaultExtras']) // if defaultExtras is set
+        ) {
+            $defaultExtrasArray = GeneralUtility::trimExplode(':', $element['TCEforms']['defaultExtras'], true);
+            foreach ($defaultExtrasArray as $part => $defaultExtrasSetting) {
+                if ($defaultExtrasSetting === 'rte_only') {
+                    // Not supported anymore
+                    unset($defaultExtrasArray[$part]);
+                } elseif ($defaultExtrasSetting === 'nowrap') {
+                    $element['TCEforms']['config']['wrap'] = 'off';
+                } elseif ($defaultExtrasSetting === 'enable-tab') {
+                    $element['TCEforms']['config']['enableTabulator'] = true;
+                } elseif ($defaultExtrasSetting === 'fixed-font') {
+                    $element['TCEforms']['config']['fixedFont'] = true;
+                } else {
+                    $this->errors[] = 'The defaultExtras setting \'' . $defaultExtrasSetting . '\' is unknown and has been dropped.';
+                }
+                unset($defaultExtrasArray[$part]);
+                $changed = true;
+            }
+        }
+
+        if ($changed) {
+            $element['TCEforms']['defaultExtras'] = implode(':', $defaultExtrasArray);
+        }
+
+        return $changed;
+    }
+
+    /**
+     * removes defaultExtra element, if empty
+     *
+     * @param array $element The field element TCA
+     * @return boolean True if changed otherwise false
+     */
+    public function cleanupEmptyDefaultExtraFields(array &$element)
+    {
+        $changed = false;
+
+        if (isset($element['TCEforms']['defaultExtras']) // if defaultExtras is set
+            && empty($element['TCEforms']['defaultExtras'])  // but is empty
+        ) {
+            unset($element['TCEforms']['defaultExtras']);
+            $changed = true;
+        }
+
+        return $changed;
+    }
+
+    protected function migrateStaticDsFilePointer()
+    {
         // Pages
         $toFix = [];
         $rows = TemplaVoilaUtility::getDatabaseConnection()->exec_SELECTgetRows(
@@ -103,9 +225,6 @@ class DataStructureV8UpdateController extends StepUpdateController
 
         $count += count($toFix);
 
-        $this->fluid->assignMultiple([
-            'count' => $count,
-            'errors' => $this->errors,
-        ]);
+        return $count;
     }
 }
