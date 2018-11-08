fac8937ee43c8b9722aacc64032686542e8acee0
Alexander Opitz <opitz@pluspol.info>
2018-11-08 14:15:35 +0100

    [FEATURE] New PageLayout module
    Rework template, css/js integration
    
    Related: #222

---
+++ Resources/Public/JavaScript/PageLayout.js
@@ -0,0 +1,20 @@
+define([
+    'jquery'
+], function($, PersistentStorage) {
+    'use strict';
+    /**
+     * @exports Ppi/TemplaVoilaPlus/PageLayout
+     */
+    var PageLayout = {
+    }
+
+    /**
+     * Initialize
+     */
+    PageLayout.initialize = function() {
+    }
+
+    $(PageLayout.initialize);
+
+    return PageLayout;
+});
