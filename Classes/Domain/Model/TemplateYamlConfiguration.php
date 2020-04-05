b4a8709672bae32f212c467790ed10fa70f7f0ac
Alexander Opitz <opitz.alexander@googlemail.com>
2020-04-05 15:17:17 +0200

    [FEATURE] Read TemplateConfiguration from Yaml file

---
+++ Classes/Domain/Model/TemplateYamlConfiguration.php
@@ -0,0 +1,97 @@
+<?php
+declare(strict_types = 1);
+namespace Ppi\TemplaVoilaPlus\Domain\Model;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use TYPO3\CMS\Core\Configuration\Loader\YamlFileLoader;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+use Ppi\TemplaVoilaPlus\Utility\TemplaVoilaUtility;
+
+/**
+ * Class to provide unique access to TemplateYamlConfiguration
+ */
+class TemplateYamlConfiguration
+{
+    /**
+     * @var \TYPO3\CMS\Core\Resource\File
+     */
+    protected $file;
+
+    /**
+     * @var string
+     */
+    protected $label = '';
+
+    /**
+     * @param \TYPO3\CMS\Core\Resource\File $file
+     */
+    public function __construct(\TYPO3\CMS\Core\Resource\File $file)
+    {
+        $this->file = $file;
+
+        // @TODO This shouldn't be here
+        $yamlFileLoader = GeneralUtility::makeInstance(YamlFileLoader::class);
+        $configuration = $yamlFileLoader->load($file->getForLocalProcessing(false));
+
+        if (!isset($configuration['tvp-template'])) {
+            throw new \Exception('No TemplaVoilà! Plus template configuration');
+        }
+
+        if (isset($configuration['tvp-template']['label'])) {
+            $this->setLabel($configuration['tvp-template']['label']);
+        }
+    }
+
+    /**
+     * Retrieve the label of the datastructure
+     *
+     * @return string
+     */
+    public function getLabel()
+    {
+        return TemplaVoilaUtility::getLanguageService()->sL($this->label);
+    }
+
+    public function setLabel(string $label)
+    {
+        $this->label = $label;
+    }
+
+    public function getIdentifier()
+    {
+        return $this->file->getIdentifier();
+    }
+
+    /**
+     * Retrieve the filereference of the template
+     *
+     * @return integer
+     */
+    public function getTstamp()
+    {
+        return $this->file->getProperty('modification_date');
+    }
+
+    /**
+     * Retrieve the filereference of the template
+     *
+     * @return integer
+     */
+    public function getCrdate()
+    {
+        return $this->file->getProperty('creation_date');
+    }
+}
