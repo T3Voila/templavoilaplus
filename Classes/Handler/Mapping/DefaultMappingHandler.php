b6c3c72e07f1cdafc65cac1c41f9eb82f28ec889
Alexander Opitz <opitz.alexander@googlemail.com>
2020-04-09 21:02:16 +0200

    [FEATURE] Implement typoscriptObjectPath for mapping

--- Classes/Handler/Mapping/DefaultMappingHandler.php
+++ Classes/Handler/Mapping/DefaultMappingHandler.php
@@ -14,7 +14,10 @@ namespace Ppi\TemplaVoilaPlus\Handler\Mapping;
  *
  * The TYPO3 project - inspiring people to share!
  */
+
+use TYPO3\CMS\Core\TypoScript\Parser\TypoScriptParser;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer;
 
 /**
  * @TODO
@@ -40,6 +43,15 @@ class DefaultMappingHandler
         foreach($mappingToTemplate as $templateFieldName => $instructions) {
             if ($instructions['fieldType'] === 'row') {
                 $processedValue = $row[$instructions['fieldName']] ? : '';
+            }
+            if ($instructions['fieldType'] === 'typoscriptObjectPath') {
+                /** @var TypoScriptParser $tsparserObj */
+                $tsparserObj = GeneralUtility::makeInstance(TypoScriptParser::class);
+                /** @var ContentObjectRenderer $cObj */
+                $cObj = GeneralUtility::makeInstance(ContentObjectRenderer::class);
+
+                list($name, $conf) = $tsparserObj->getVal($instructions['fieldName'], $GLOBALS['TSFE']->tmpl->setup);
+                $processedValue = $cObj->cObjGetSingle($name, $conf, 'TemplaVoila_ProcObjPath--' . str_replace('.', '*', $instructions['fieldName']) . '.');
             }
             $processedMapping[$templateFieldName] = $processedValue;
         }
