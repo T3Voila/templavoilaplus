d1ac3bc3fb6b6d3b02cbdcf14e386c1d769c4c45
Alexander Opitz <opitz@extrameile.agency>
2020-04-17 22:14:13 +0200

    [TASK] Add error message if a page has no mapping configuration, add a warning if upgrade script didn't run.

--- Classes/Controller/Backend/Handler/DoktypeDefaultHandler.php
+++ Classes/Controller/Backend/Handler/DoktypeDefaultHandler.php
@@ -56,16 +56,33 @@ class DoktypeDefaultHandler
             $pageRecord['tx_templavoilaplus_map'] = $apiService->getMapIdentifierFromRootline($rootLine);
         }
 
-        $mappingConfiguration = ApiHelperUtility::getMappingConfiguration($pageRecord['tx_templavoilaplus_map']);
-        $combinedBackendLayoutConfigurationIdentifier = $mappingConfiguration->getCombinedBackendLayoutConfigurationIdentifier();
+        if (!$pageRecord['tx_templavoilaplus_map']) {
+            $controller->getView()->getModuleTemplate()->addFlashMessage(
+                'No mapping configuration found for this page. Please edit the page properties and select one.',
+                'No mapping configuration found',
+                \TYPO3\CMS\Core\Messaging\AbstractMessage::ERROR,
+                false
+            );
+            if (!empty($pageRecord['tx_templavoilaplus_ds']) || !empty($pageRecord['tx_templavoilaplus_next_ds'])) {
+                $controller->getView()->getModuleTemplate()->addFlashMessage(
+                    'Older configuration found, did you upgrade to "TemplaVoilà! Plus 8" but forgot to run the upgrade scripts?',
+                    'Did you forgot to Upgrade',
+                    \TYPO3\CMS\Core\Messaging\AbstractMessage::WARNING,
+                    false
+                );
+            }
+        } else {
+            $mappingConfiguration = ApiHelperUtility::getMappingConfiguration($pageRecord['tx_templavoilaplus_map']);
+            $combinedBackendLayoutConfigurationIdentifier = $mappingConfiguration->getCombinedBackendLayoutConfigurationIdentifier();
 
-        if ($combinedBackendLayoutConfigurationIdentifier === '') {
-            $combinedBackendLayoutConfigurationIdentifier = 'TVP\BackendLayout:DefaultPage.tvp.yaml';
+            if ($combinedBackendLayoutConfigurationIdentifier === '') {
+                $combinedBackendLayoutConfigurationIdentifier = 'TVP\BackendLayout:DefaultPage.tvp.yaml';
+            }
+            $backendLayoutConfiguration = ApiHelperUtility::getBackendLayoutConfiguration($combinedBackendLayoutConfigurationIdentifier);
+
+
+            $controller->addContentPartial('body', 'Backend/Handler/DoktypeDefaultHandler'); // @TODO Add them automagically in controller to harden naming?
         }
-        $backendLayoutConfiguration = ApiHelperUtility::getBackendLayoutConfiguration($combinedBackendLayoutConfigurationIdentifier);
-
-
-        $controller->addContentPartial('body', 'Backend/Handler/DoktypeDefaultHandler'); // @TODO Add them automagically in controller to harden naming?
 
         return '';
     }
