d50f4975f9ae417963fab2be1293fab492c71492
Alexander Opitz <opitz@extrameile.agency>
2020-04-19 14:48:51 +0200

    [TASK] Start with the informational part of the TV+ 8 Update Script

--- Classes/Controller/Backend/Update/TemplaVoilaPlus8UpdateController.php
+++ Classes/Controller/Backend/Update/TemplaVoilaPlus8UpdateController.php
@@ -27,15 +27,67 @@ class TemplaVoilaPlus8UpdateController extends StepUpdateController
 {
     protected $errors = [];
 
+    /**
+     * Introduction
+     */
     protected function stepStart()
     {
-        $this->fluid->assignMultiple([
-            'storagePidConversationNeeded' => $this->storagePidConversationNeeded(),
-        ]);
+        // Check sys_registry if we already run?
+        // Not realy, this must be done in the SwitchUpdateController who will disable our button
+        // So we could provide a way to start this task again.
+    }
+
+    /**
+     * Static DS or Database
+     */
+    protected function step1()
+    {
+        // Check for existence of fields tx_templavoilaplus_ds/tx_templavoilaplus_to on pages/tt_content
+        // Check of existence of table tx_templavoila_datastructure and tx_templavoila_tmplobj
+        // Check for configuration staticDS = 1 and content of the configured paths
+        // Check for storage_pid's to determine how much extensions we need to generate and/or need mapping into Site Management
+        // Check database if the found ds/to are in usage, give the possibility to delete them?
+    }
+
+    /**
+     * Find extension names / possible theme extensions / create own theme extension
+     */
+    protected function step2()
+    {
+        // Check extension names and provide form to create new extension or better
+    }
+
+    /**
+     * Build new extension (or replace existing one) or multiple for multiple designs
+     * Or add them to Site Management directories (if support is implemented)
+     * The place may depend if you use composer installed TYPO3 or package based TYPO3
+     */
+    protected function step3()
+    {
+        // Create files and folders
+    }
+
+    /**
+     * Register the generated extensions
+     * Update the map field with the configuration (depending on ds/to)
+     */
+    protected function step4()
+    {
+        // Register extensions
+        // Update pages
+        // Update tt_content
     }
 
     protected function stepFinal()
     {
+        // Write into sys_registry
+    }
+
+    protected function stepTODO()
+    {
+        $this->fluid->assignMultiple([
+            'storagePidConversationNeeded' => $this->storagePidConversationNeeded(),
+        ]);
     }
 
     protected function storagePidConversationNeeded(): bool
