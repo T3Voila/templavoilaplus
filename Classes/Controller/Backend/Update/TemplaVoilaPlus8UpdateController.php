1b0ebec025adb919da2a9382ae913deb555d7730
Alexander Opitz <opitz@extrameile.agency>
2020-04-20 13:08:19 +0200

    [TASK] Validate DS and TO data

--- Classes/Controller/Backend/Update/TemplaVoilaPlus8UpdateController.php
+++ Classes/Controller/Backend/Update/TemplaVoilaPlus8UpdateController.php
@@ -65,25 +65,41 @@ class TemplaVoilaPlus8UpdateController extends StepUpdateController
             $storagePidsAreFine = true;
         }
 
-        $staticDsEnabled = false;
+        // Search for all DS configurations
+        $useStaticDS = false;
         if ($this->extConf['staticDS']['enable'])
         {
             // Load all DS from path
-            $staticDsEnabled = true;
+            $useStaticDS = true;
+            $allDs = $this->getAllDsFromStatic();
         } else {
             // Load DS from DB
+            /** @TODO Implement */
         }
 
         // Search for all TO configurations
+        $allTo = $this->getAllToFromDB();
+
+        $allDsToValid = false;
+        list($validationErrors, $validatedDs, $validatedToWithDs) = $this->checkAllDsToValid($allDs, $allTo);
+        if (count($validationErrors) === 0) {
+            $allDsToValid = true;
+        }
+
 
         // Check database if the found ds/to are in usage, give the possibility to delete them?
 
-        $allChecksAreFine = $allOldDatabaseElementsFound && $allNewDatabaseElementsFound && $storagePidsAreFine;
+        $allChecksAreFine = $allOldDatabaseElementsFound && $allNewDatabaseElementsFound && $storagePidsAreFine && $allDsToValid;
 
         $this->fluid->assignMultiple([
             'allOldDatabaseElementsFound' => $allOldDatabaseElementsFound,
             'allNewDatabaseElementsFound' => $allNewDatabaseElementsFound,
             'storagePidsAreFine' => $storagePidsAreFine,
+            'useStaticDS' => $useStaticDS,
+            'allDsToValid' => $allDsToValid,
+            'validationErrors' => $validationErrors,
+            'validatedDs' => $validatedDs,
+            'validatedToWithDs' => $validatedToWithDs,
             'allChecksAreFine' => $allChecksAreFine,
         ]);
     }
@@ -118,6 +134,153 @@ class TemplaVoilaPlus8UpdateController extends StepUpdateController
             ->getConnectionForTable($tablename)
             ->getSchemaManager()
             ->listTableColumns($tablename);
+    }
+
+    protected function getAllDsFromStatic(): array
+    {
+        $allDs = [];
+
+        $systemPath = $this->getSystemPath();
+
+        $paths = array_unique(array('fce' => $this->extConf['staticDS']['path_fce'], 'page' => $this->extConf['staticDS']['path_page']));
+        foreach ($paths as $type => $path) {
+            $absolutePath = GeneralUtility::getFileAbsFileName($path);
+            $files = GeneralUtility::getFilesInDir($absolutePath, 'xml', true);
+            // if all files are in the same folder, don't resolve the scope by path type
+            if (count($paths) == 1) {
+                $type = false;
+            }
+            foreach ($files as $filePath) {
+                $staticDataStructure = [];
+                $pathInfo = pathinfo($filePath);
+
+                $staticDataStructure['title'] = $pathInfo['filename'];
+                $staticDataStructure['path'] = substr($filePath, strlen($systemPath));
+                $iconPath = $pathInfo['dirname'] . '/' . $pathInfo['filename'] . '.gif';
+                if (file_exists($iconPath)) {
+                    $staticDataStructure['icon'] = substr($iconPath, strlen($systemPath));
+                }
+
+                if (($type !== false && $type === 'fce') || strpos($pathInfo['filename'], '(fce)') !== false) {
+                    $staticDataStructure['scope'] = \Ppi\TemplaVoilaPlus\Domain\Model\Scope::SCOPE_FCE;
+                } else {
+                    $staticDataStructure['scope'] = \Ppi\TemplaVoilaPlus\Domain\Model\Scope::SCOPE_PAGE;
+                }
+
+                $allDs[] = $staticDataStructure;
+            }
+        }
+        return $allDs;
+    }
+
+    protected function getAllToFromDB(): array
+    {
+        $allTo = [];
+
+        $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)
+            ->getQueryBuilderForTable('tx_templavoilaplus_tmplobj');
+        $queryBuilder
+            ->getRestrictions()
+            ->removeAll()
+            ->add(GeneralUtility::makeInstance(DeletedRestriction::class));
+
+        $result = $queryBuilder
+            ->select('*')
+            ->from('tx_templavoilaplus_tmplobj')
+            ->orderBy('pid')
+            ->execute()
+            ->fetchAll();
+
+        return $result;
+    }
+
+    protected function checkAllDsToValid(array $allDs, array $allTo): array
+    {
+        $validatedToWithDs = [];
+        $validatedDs = [];
+        $validationErrors = [];
+
+        $systemPath = $this->getSystemPath();
+
+        libxml_use_internal_errors(true);
+
+        foreach ($allDs as $ds) {
+            /** @TODO Implement NonStaticDs */
+            if (is_file($systemPath . $ds['path']) && is_readable($systemPath . $ds['path'])) {
+                $ds['haveTo'] = 0;
+                $ds['valid'] = false;
+                $result = simplexml_load_file($systemPath . $ds['path']);
+                if ($result === false) {
+                    $errors = libxml_get_errors();
+                    $validationErrors[] = 'Cannot verify XML of DS with title "' . $ds['title'] . '" Error is: ' . reset($errors)->message;
+                } else {
+                    $ds['valid'] = true;
+                }
+            } else {
+                $validationErrors[] = 'Cannot verify DS with title "' . $ds['title'] . '", as file "' . $ds['path'] . '" could not be found or isn\'t readable';
+            }
+            $validatedDs[$ds['path']] = $ds;
+        }
+
+        foreach ($allTo as $to) {
+            $to['valid'] = false;
+            if (
+                (!empty($to['datastructure']) && isset($validatedDs[$to['datastructure']]))
+                || $to['parent'] > 0
+            ) {
+                if ($to['parent'] === 0) {
+                    $validatedDs[$to['datastructure']]['haveTo']++;
+                }
+
+                $templatefile = GeneralUtility::getFileAbsFileName($to['fileref']);
+                if (is_file($templatefile) && is_readable($templatefile)) {
+                    if (!empty($to['templatemapping']) && !is_array(unserialize($to['templatemapping']))) {
+                        $validationErrors[] = 'Cannot verify TO with title "' . $to['title'] . '" and uid "' . $to['uid'] . '", as mapping is broken.';
+                    } else {
+                        $to['valid'] = true;
+                        $to['DS'] = $validatedDs[$to['datastructure']]; /** @TODO If parent then from parent! Check if parent exists */
+                    }
+                } else {
+                    $validationErrors[] = 'Cannot verify TO with title "' . $to['title'] . '" and uid "' . $to['uid'] . '", as template file "' . $to['fileref'] . '" could not be found.';
+                }
+            } else {
+                $validationErrors[] = 'Cannot verify TO with title "' . $to['title'] . '" and uid "' . $to['uid'] . '", as DS could not be found.';
+            }
+            $validatedToWithDs[$to['uid']] = $to;
+        }
+
+        foreach ($validatedToWithDs as $key => $validTo) {
+            if ($validTo['parent'] > 0) {
+                if (isset($validatedToWithDs[$validTo['parent']])) {
+                    $validatedToWithDs[$validTo['parent']]['childTO'][] = $validTo;
+                    unset($validatedToWithDs[$key]);
+                } else {
+                    $validatedToWithDs[$key]['valid'] = false;
+                    $validationErrors[] = 'Cannot verify TO with title "' . $validTo['title'] . '" and uid "' . $validTo['uid'] . '", as parent Template Object could not be found.';
+                }
+            }
+        }
+
+        foreach ($validatedDs as $key => $ds) {
+            if ($ds['haveTo'] === 0) {
+                $validatedDs[$key]['valid'] = false;
+                $validationErrors[] = 'Cannot verify DS with title "' . $ds['title'] . '" it has no Template Object data';
+            }
+        }
+
+        return [$validationErrors, $validatedDs, $validatedToWithDs];
+    }
+
+    protected function getSystemPath(): string
+    {
+        $systemPath = '/';
+
+        if (version_compare(TYPO3_version, '9.2.0', '>=')) {
+            $systemPath = \TYPO3\CMS\Core\Core\Environment::getPublicPath();
+        } else {
+            $systemPath = rtrim(PATH_site, '/');
+        }
+        return $systemPath . '/';
     }
 
     /**
