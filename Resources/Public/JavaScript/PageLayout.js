dfd6979ec912ae998ed61ec7e369a31ddfb64928
Alexander Opitz <opitz@extrameile.agency>
2021-01-08 23:15:05 +0100

    [FEATURE] PageLayout - Add right sidebar
    Implement the right sidebar for the upcomming tools using Tooltipster
    for the toolpalettes (later). Demonstration of the functionality is
    implemented with an switcher for normal/dark mode. Also implementing
    dark mode functionality (in hope it looks nice).
    
    Related: #222

--- Resources/Public/JavaScript/PageLayout.js
+++ Resources/Public/JavaScript/PageLayout.js
@@ -1,5 +1,6 @@
 define([
-    'jquery'
+    'jquery',
+    'TYPO3/CMS/Templavoilaplus/Tooltipster'
 ], function($) {
     'use strict';
     /**
@@ -14,6 +15,32 @@ define([
     PageLayout.initialize = function() {
         $('#moduleWrapper').removeClass('hidden');
         $('#moduleLoadingIndicator').addClass('hidden');
+
+        $('#navbarConfig').tooltipster({
+            theme: 'tooltipster-noir',
+            side: 'left',
+            interactive: true,
+            trigger: 'click'
+        });
+        $('#dark-mode-switch').change(function() {
+            if (this.checked) {
+                $('body').addClass('animationTransition');
+                setTimeout(() => {
+                    $('body').addClass('dark-mode-on');
+                }, 150);
+                setTimeout(() => {
+                    $('body').removeClass('animationTransition');
+                }, 500);
+            } else {
+                $('body').addClass('animationTransition');
+                setTimeout(() => {
+                    $('body').removeClass('dark-mode-on');
+                }, 150);
+                setTimeout(() => {
+                    $('body').removeClass('animationTransition');
+                }, 500);
+            }
+        });
     }
 
     $(PageLayout.initialize);
