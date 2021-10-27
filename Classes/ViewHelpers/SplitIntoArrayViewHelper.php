b70df5efed454725f1c1ba4e7aea61f9af7c22b5
Alexander Opitz <opitz@extrameile.agency>
2021-10-27 18:24:37 +0200

    PageLayoutModul: Introduce SplitIntoArrayViewHelper
    Splits a string on a char by using php mb_split
    
    Related: #302
    Release: 8.0.0

---
+++ Classes/ViewHelpers/SplitIntoArrayViewHelper.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tvp\TemplaVoilaPlus\ViewHelpers;
+
+use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
+use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
+use TYPO3Fluid\Fluid\Core\ViewHelper\Traits\CompileWithContentArgumentAndRenderStatic;
+
+/**
+ * Like VariableViewHelper but against an array
+ */
+class SplitIntoArrayViewHelper extends AbstractViewHelper
+{
+    use CompileWithContentArgumentAndRenderStatic;
+
+    /**
+     * @return void
+     */
+    public function initializeArguments()
+    {
+        $this->registerArgument('value', 'mixed', 'Value to assign. If not in arguments then taken from tag content');
+        $this->registerArgument('pattern', 'string', 'The delimiter/seperator for the string as regex (mostly \R) without delimiter', true);
+        $this->registerArgument('limit', 'string', 'It will be split in limit elements as maximum');
+    }
+
+    /**
+     * @param array $arguments
+     * @param \Closure $renderChildrenClosure
+     * @param RenderingContextInterface $renderingContext
+     * @return null
+     */
+    public static function renderStatic(
+        array $arguments,
+        \Closure $renderChildrenClosure,
+        RenderingContextInterface $renderingContext
+    ) {
+        $value = $arguments['value'] ?? $renderChildrenClosure();
+        $limit = $arguments['limit'] ?? -1; // mb_split default is -1
+
+        $result = mb_split($arguments['pattern'], $value, $limit);
+
+        // Return empty array for false
+        if ($result === false) {
+            return [];
+        }
+        return $result;
+    }
+}
