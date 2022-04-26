83147a3fb2d083612a2ec45fc808f274de6a1d1b
J. Peter M. Schuler <j.peter.m.schuler@uni-due.de>
2022-04-27 01:18:04 +0200

    [TASK] remove documentation from page tsconfig in favor of new docs

--- Configuration/TSConfig/Page.ts
+++ Configuration/TSConfig/Page.ts
@@ -4,13 +4,3 @@ mod.web_txtemplavoilaplusLayout.enableContentAccessWarning = 1
 mod.web_txtemplavoilaplusLayout.enableLocalizationLinkForFCEs = 0
 mod.web_txtemplavoilaplusLayout.useLiveWorkspaceForReferenceListUpdates = 1
 mod.web_txtemplavoilaplusLayout.adminOnlyPageStructureInheritance = fallback
-
-# example for filtering allowed maps allow single string or array
-# refers to mappingconfiguration identifier, so look for the prefix in
-# yourextenstion/Configuration/TVP/MappingPlaces.php for an identifier like
-# vendor/yourextension/Page/MappingConfiguration
-# and use that like:
-# mod.web_txtemplavoilaplusLayout.filterMaps = vendor/yourextension
-# or like:
-# mod.web_txtemplavoilaplusLayout.filterMaps.1 = vendor/yourextension/Page
-# mod.web_txtemplavoilaplusLayout.filterMaps.2 = vendor/yourotherextension/FCE
