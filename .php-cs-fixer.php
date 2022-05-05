402017b00b61c3ee4f744a4a60c2d288e8f62c12
J. Peter M. Schuler <j.peter.m.schuler@uni-due.de>
2022-05-05 11:08:01 +0200

    [TASK] add explicit rule for phpdoc align

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -1,6 +1,8 @@
 <?php
 
 $config = \TYPO3\CodingStandards\CsFixerConfig::create();
-$config->getFinder()->in('Classes')->in('Configuration');
+$config->getFinder()->in('Classes')->in('Configuration')->in('Tests');
 $config->getFinder()->exclude(['Classes/Form']);
-return $config;
+return $config->setRules([
+    'phpdoc_align' => ['align' => 'left']
+]);
