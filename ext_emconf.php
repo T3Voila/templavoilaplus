4a05fe59da878f0beb97ebebb8e8bc9c11c8a47f
Alexander Opitz <opitz@extrameile.agency>
2022-05-07 11:04:14 +0200

    [RELEASE] Release beta 3 as 8.0.3
    Related: #361
    Releases: 8.0.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,7 +4,7 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'TemplaVoilà! Plus',
     'description' => 'Point-and-click, popular and easy template engine for TYPO3. Replacement for old TemplaVoilà!.',
     'category' => 'misc',
-    'version' => '8.0.0-beta-2',
+    'version' => '8.0.3',
     'state' => 'beta',
     'uploadfolder' => 0,
     'createDirs' => 'uploads/tx_templavoilaplus/',
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => 'Extrameile GmbH',
     'constraints' => [
         'depends' => [
-            'php' => '7.2.0-7.4.99',
+            'php' => '7.2.0-8.1.99',
             'typo3' => '8.7.0-11.5.99',
             'install' => '8.7.0-11.5.99',
         ],
