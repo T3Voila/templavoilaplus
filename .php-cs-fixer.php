b9930ed4e475fbdc52d8a30f4d3326c175ea8073
J. Peter M. Schuler <j.peter.m.schuler@uni-due.de>
2022-10-23 00:43:09 +0200

    [TASK] adapt php-cs-fixer to exclude codeception autogenerated files

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -2,7 +2,7 @@
 
 $config = \TYPO3\CodingStandards\CsFixerConfig::create();
 $config->getFinder()->in('Classes')->in('Configuration')->in('Tests');
-$config->getFinder()->exclude(['Classes/Form']);
+$config->getFinder()->exclude(['Form','Acceptance/Support/_generated']);
 return $config->setRules([
     'phpdoc_align' => ['align' => 'left']
 ]);
