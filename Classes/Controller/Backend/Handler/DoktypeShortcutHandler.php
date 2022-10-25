e60d371e58ae45e218d33f995b580ddbeb705770
J. Peter M. Schuler <j.peter.m.schuler@uni-due.de>
2022-10-25 10:03:21 +0200

    [TASK] fix missing ::class
    fixes #463

--- Classes/Controller/Backend/Handler/DoktypeShortcutHandler.php
+++ Classes/Controller/Backend/Handler/DoktypeShortcutHandler.php
@@ -43,7 +43,7 @@ class DoktypeShortcutHandler extends AbstractDoktypeHandler
 
         $pageRepositoryClass = \TYPO3\CMS\Core\Domain\Repository\PageRepository::class;
         if (version_compare(TYPO3_version, '10.0.0', '<')) {
-            $pageRepositoryClass = \TYPO3\CMS\Frontend\Page\PageRepository;
+            $pageRepositoryClass = \TYPO3\CMS\Frontend\Page\PageRepository::class;
         }
 
         switch ($shortcutMode) {
