8a9bfed29d7064185ddc7f24ee51105bb9c9910c
Alexander Opitz <opitz.alexander@googlemail.com>
2023-03-23 22:17:40 +0100

    [FEATURE] Update basics for v11/v12 compatibility Feature
    Fix branch alias and php versions
    
    Related: #497
    Release: 11.0.0

--- Migrations/Code/ClassAliasMap.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return [
-    'Ppi\\TemplaVoilaPlus\\Controller\\FrontendController' => \Tvp\TemplaVoilaPlus\Controller\Frontend\FrontendController::class,
-    'Ppi\\TemplaVoilaPlus\\Controller\\SectionIndexController' => \Tvp\TemplaVoilaPlus\Controller\SectionIndexController::class
-];
