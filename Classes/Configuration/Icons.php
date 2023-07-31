f21f6e9d2414effbf80e3faa3cabced6a18f07e7
J. Peter M. Schuler <j.peter.m.schuler@uni-due.de>
2023-07-31 11:15:15 +0200

    [BUGFIX] 12LTS doesn't use ext_tables.php for module and icon registration

---
+++ Classes/Configuration/Icons.php
@@ -0,0 +1,60 @@
+<?php
+
+return [
+    'extensions-templavoila-paste' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class,
+        'source' => 'EXT:templavoilaplus/Resources/Public/Icon/clip_pasteafter.gif'
+    ],
+    'extensions-templavoila-pasteSubRef' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class,
+        'source' => 'EXT:templavoilaplus/Resources/Public/Icon/clip_pastesubref.gif'
+    ],
+    'extensions-templavoila-makelocalcopy' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class,
+        'source' => 'EXT:templavoilaplus/Resources/Public/Icon/makelocalcopy.gif'
+    ],
+    'extensions-templavoila-clip_ref' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class,
+        'source' => 'EXT:templavoilaplus/Resources/Public/Icon/clip_ref.gif'
+    ],
+    'extensions-templavoila-clip_ref-release' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class,
+        'source' => 'EXT:templavoilaplus/Resources/Public/Icon/clip_ref_h.gif'
+    ],
+    'extensions-templavoila-htmlvalidate' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class,
+        'source' => 'EXT:templavoilaplus/Resources/Public/Icon/html_go.png'
+    ],
+    'extensions-templavoila-type-fce' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class,
+        'source' => 'EXT:templavoilaplus/Resources/Public/Icon/icon_fce_ce.png'
+    ],
+    'extensions-templavoila-template-default' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:templavoilaplus/Resources/Public/Icons/TemplateDefault.svg'
+    ],
+    'extensions-templavoila-datastructure-default' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:templavoilaplus/Resources/Public/Icons/DataStructureDefault.svg'
+    ],
+    'extensions-templavoila-folder' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:templavoilaplus/Resources/Public/Icons/Folder.svg'
+    ],
+    'extensions-templavoila-menu-item' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:templavoilaplus/Resources/Public/Icons/MenuItem.svg'
+    ],
+    'extensions-templavoila-unlink' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:templavoilaplus/Resources/Public/Icons/Unlink.svg'
+    ],
+    'extensions-templavoila-pagemodule' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:templavoilaplus/Resources/Public/Icons/PageModuleIcon.svg'
+    ],
+    'extensions-templavoila-administrationmodule' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:templavoilaplus/Resources/Public/Icons/AdministrationModuleIcon.svg'
+    ],
+];
---
+++ Configuration/Backend/Modules.php
@@ -0,0 +1,79 @@
+<?php
+
+return [
+    'web_layouttvp' => [
+        'parent' => 'web',
+        'position' => 'top',
+        'access' => 'user',
+        'workspaces' => 'live',
+        'path' => 'module/web/layouttvp',
+        'labels' => 'LLL:EXT:templavoilaplus/Resources/Private/Language/Backend/PageLayout.xlf',
+        'iconIdentifier' => 'extensions-templavoila-pagemodule',
+        'extensionName' => 'Templavoilaplus',
+        'navigationComponentId' => 'TYPO3/CMS/Backend/PageTree/PageTreeElement',
+        'controllerActions' => [
+            \Tvp\TemplaVoilaPlus\Controller\Backend\PageLayoutController::class => [
+                'flash', 'tree', 'clipboard', 'links', 'fileReference', 'fileReferenceCreate',
+            ],
+        ],
+    ],
+    'tools_controlcenter' => [
+        'parent' => 'tools',
+        'position' => ['top'],
+        'access' => 'user',
+        'workspaces' => 'live',
+        'path' => '/module/tools/controlcenter',
+        'labels' => 'LLL:EXT:examples/Resources/Private/Language/AdminModule/locallang_mod.xlf',
+        'iconIdentifier' => 'extensions-templavoila-administrationmodule',
+        'extensionName' => 'Templavoilaplus',
+        'inheritNavigationComponentFromMainModule' => false,
+        'navigationComponentId' => '',
+        'controllerActions' => [
+            \Tvp\TemplaVoilaPlus\Controller\Backend\ControlCenterController::class => [
+                'show',
+                'debug'
+            ],
+            \Tvp\TemplaVoilaPlus\Controller\Backend\ControlCenter\DataStructuresController::class => [
+                'list',
+                'info',
+                'delete'
+            ],
+            \Tvp\TemplaVoilaPlus\Controller\Backend\ControlCenter\MappingsController::class => [
+                'list'
+            ],
+            \Tvp\TemplaVoilaPlus\Controller\Backend\ControlCenter\TemplatesController::class => [
+                'list',
+                'info'
+            ],
+            \Tvp\TemplaVoilaPlus\Controller\Backend\ControlCenter\UpdateController::class => [
+                'info'
+            ],
+            \Tvp\TemplaVoilaPlus\Controller\Backend\ControlCenter\Update\TemplaVoilaPlus8Controller::class => [
+                'stepStart',
+                'step1',
+                'step2',
+                'step3',
+                'step3NewExtension',
+                'step3ExistingExtension',
+                'step4',
+                'step5',
+                'stepFinal'
+            ],
+            \Tvp\TemplaVoilaPlus\Controller\Backend\ControlCenter\Update\ServerMigrationController::class => [
+                'stepStart'
+            ],
+            \Tvp\TemplaVoilaPlus\Controller\Backend\ControlCenter\Update\DataStructureV8Controller::class => [
+                'stepStart',
+                'stepFinal'
+            ],
+            \Tvp\TemplaVoilaPlus\Controller\Backend\ControlCenter\Update\DataStructureV10Controller::class => [
+                'stepStart',
+                'stepFinal'
+            ],
+            \Tvp\TemplaVoilaPlus\Controller\Backend\ControlCenter\Update\DataStructureV11Controller::class => [
+                'stepStart',
+                'stepFinal'
+            ],
+        ],
+    ],
+];
