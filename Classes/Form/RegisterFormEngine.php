1ab7422ad870336792b530cee15ecf6c09f635f1
Alexander Opitz <opitz.alexander@googlemail.com>
2023-10-13 14:10:05 +0200

    [TASK] Remove FlexFormManipulations
    Remove all FlexForm manipulations which were related to support
    own form of multilanguage lDEV/vDEV. We need to get rid of this
    and it seams, nobody is using TV+ with this option.
    
    Related: #497
    Release: 12.0

--- Classes/Form/RegisterFormEngine.php
+++ Classes/Form/RegisterFormEngine.php
@@ -23,64 +23,7 @@ class RegisterFormEngine
 {
     public function processEvent(BootCompletedEvent $event)
     {
-        $this->registerFormEngineContainer();
-        $this->registerFormEngineProviders();
         $this->registerHookFormEngine();
-    }
-
-    public function registerFormEngineContainer()
-    {
-        // Register language aware flex form handling in FormEngine
-        // Register render elements
-        $GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1443361297] = [
-            'nodeName' => 'flex',
-            'priority' => 40,
-            'class' => \Tvp\TemplaVoilaPlus\Form\Container\FlexFormEntryContainer::class,
-        ];
-        $GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1443361298] = [
-            'nodeName' => 'flexFormNoTabsContainer',
-            'priority' => 40,
-            'class' => \Tvp\TemplaVoilaPlus\Form\Container\FlexFormNoTabsContainer::class,
-        ];
-        $GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1443361299] = [
-            'nodeName' => 'flexFormTabsContainer',
-            'priority' => 40,
-            'class' => \Tvp\TemplaVoilaPlus\Form\Container\FlexFormTabsContainer::class,
-        ];
-        $GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1443361300] = [
-            'nodeName' => 'flexFormElementContainer',
-            'priority' => 40,
-            'class' => \Tvp\TemplaVoilaPlus\Form\Container\FlexFormElementContainer::class,
-        ];
-        $GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1443361301] = [
-            'nodeName' => 'flexFormSectionContainer',
-            'priority' => 40,
-            'class' => \Tvp\TemplaVoilaPlus\Form\Container\FlexFormSectionContainer::class,
-        ];
-    }
-
-    public function registerFormEngineProviders()
-    {
-        // Unregister stock TcaFlex* data provider and substitute with own data provider at the same dependency position
-        \Tvp\TemplaVoilaPlus\Utility\FormEngineUtility::replaceInFormDataGroups(
-            [
-                \TYPO3\CMS\Backend\Form\FormDataProvider\TcaFlexProcess::class
-                    => \Tvp\TemplaVoilaPlus\Form\FormDataProvider\TcaFlexProcess::class,
-                \TYPO3\CMS\Backend\Form\FormDataProvider\TcaFlexFetch::class
-                    => \Tvp\TemplaVoilaPlus\Form\FormDataProvider\TcaFlexFetch::class,
-                \TYPO3\CMS\Backend\Form\FormDataProvider\TcaFlexPrepare::class
-                    => \Tvp\TemplaVoilaPlus\Form\FormDataProvider\TcaFlexPrepare::class,
-            ]
-        );
-        \Tvp\TemplaVoilaPlus\Utility\FormEngineUtility::replaceInFormDataGroups(
-            [
-                \TYPO3\CMS\Backend\Form\FormDataProvider\EvaluateDisplayConditions::class
-                    => \Tvp\TemplaVoilaPlus\Form\FormDataProvider\EvaluateDisplayConditions::class,
-            ]
-        );
-        // In TYPO3 8 there is no TcaFlexFetch, so readd it.
-        // @TODO Remerge Core
-        \Tvp\TemplaVoilaPlus\Utility\FormEngineUtility::addTcaFlexFetch();
     }
 
     public function registerHookFormEngine()
--- ext_localconf.php
+++ ext_localconf.php
@@ -3,11 +3,6 @@
 defined('TYPO3') or die();
 // Unserializing the configuration so we can use it here
 
-// Register "XCLASS" of FlexFormTools for language parsing
-// Done also in TableConfigurationPostProcessingHook!
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\TYPO3\CMS\Core\Configuration\FlexForm\FlexFormTools::class]['className']
-    = \Tvp\TemplaVoilaPlus\Configuration\FlexForm\FlexFormTools8::class;
-
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][TYPO3\CMS\Core\Page\JavaScriptModuleInstruction::class]['className']
     = Tvp\TemplaVoilaPlus\Core\Page\JavaScriptModuleInstruction::class;
 
