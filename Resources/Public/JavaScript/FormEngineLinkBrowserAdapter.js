425bea0099b7f7f8283d58a7a75677ed9c95a51b
Alexander Opitz <opitz.alexander@googlemail.com>
2024-01-25 00:24:46 +0100

    [BUGFIX] FormEnginLinkBrowserAdapter fix recognition of modal
    If we do not edit content in the TV+ modal we shouldn't recognize the
    LinkHandler modal as our own.
    
    Resolves: #551
    Release: 12.0.1 8.2.0

--- Resources/Public/JavaScript/FormEngineLinkBrowserAdapter.js
+++ Resources/Public/JavaScript/FormEngineLinkBrowserAdapter.js
@@ -92,10 +92,10 @@ define(['jquery', 'TYPO3/CMS/Recordlist/LinkBrowser', 'TYPO3/CMS/Backend/Modal']
     if (
        typeof window.parent !== 'undefined' &&
        typeof window.parent.frames.list_frame !== 'undefined' &&
-       window.parent.frames.list_frame.parent.document.querySelector('.t3js-modal-iframe') !== null &&
-       window.parent.frames.list_frame.parent.document.querySelector('.t3js-modal-iframe').getRootNode().querySelector('body.modal-open') !== null
+       window.parent.frames.list_frame.parent.document.querySelector('.tvp-modal-record-edit .t3js-modal-iframe') !== null &&
+       window.parent.frames.list_frame.parent.document.querySelector('.tvp-modal-record-edit .t3js-modal-iframe').getRootNode().querySelector('body.modal-open') !== null
      ) {
-       opener = window.parent.frames.list_frame.parent.document.querySelector('.t3js-modal-iframe').contentWindow;
+       opener = window.parent.frames.list_frame.parent.document.querySelector('.tvp-modal-record-edit .t3js-modal-iframe').contentWindow;
      } else if (
       typeof window.parent !== 'undefined' &&
       typeof window.parent.document.list_frame !== 'undefined' &&
