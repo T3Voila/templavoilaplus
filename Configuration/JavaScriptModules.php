bc215f6b204348bfab22323bb31e22e2ee9bc804
Alexander Opitz <opitz.alexander@googlemail.com>
2025-10-06 11:09:27 +0200

    [FEATURE] TYPO3 v13: Fix JavaScriptModules configuration
    Related: #609
    Release: 12.0.4

--- Configuration/JavaScriptModules.php
+++ Configuration/JavaScriptModules.php
@@ -7,8 +7,8 @@ return [
             'path' => 'EXT:templavoilaplus/Resources/Public/JavaScript/',
             // Exclude files of the following folders from being import-mapped
             'exclude' => [
-                'EXT:templavoila/Resources/Public/JavaScript/Contrib/',
-                'EXT:templavoila/Resources/Public/JavaScript/Overrides/',
+                'EXT:templavoilaplus/Resources/Public/JavaScript/Contrib/',
+                'EXT:templavoilaplus/Resources/Public/JavaScript/Overrides/',
             ],
         ],
         // Overriding a file from another package
