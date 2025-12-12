00e9b33fb252a21d83bb10309ec08008fb12126a
Alexander Opitz <opitz.alexander@googlemail.com>
2025-12-12 10:45:46 +0100

    [FEATURE] TYPO3 v13: Mark TV+ as TYPO3 v13 compatible
    Resolve: #609
    Release: 12.0.4

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,7 +4,7 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'TemplaVoilà! Plus',
     'description' => 'Building kit for custom  pages and content elements with individual fields, containers and backend layouts. Supporting drag\'n\'drop and multiple references.',
     'category' => 'misc',
-    'version' => '12.0.3',
+    'version' => '12.0.3-dev',
     'state' => 'beta',
     'clearcacheonload' => 1,
     'author' => 'Alexander Opitz',
@@ -13,8 +13,8 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'php' => '8.1.0-8.4.99',
-            'typo3' => '12.4.0-12.4.99',
-            'install' => '12.4.0-12.4.99',
+            'typo3' => '12.4.0-13.4.99',
+            'install' => '12.4.0-13.4.99',
         ],
     ],
 ];
